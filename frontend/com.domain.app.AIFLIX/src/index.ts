a355ce8acbf6c6d598cde279e9c31c1a271d3ced
KerekesArpadAkos <kerekes.arpad.akos@student.ms.sapientia.ro>
2024-10-28 13:07:12 +0200

    Removed vite, because it caused a lot of issues and bugs.

--- frontend/com.domain.app.AIFLIX/src/index.ts
+++ frontend/com.domain.app.AIFLIX/src/index.ts
@@ -1,32 +1,10 @@
-import { Launch } from '@lightningjs/sdk';
-import { App } from './App';
+import { Lightning, Launch, PlatformSettings, AppData } from "@lightningjs/sdk";
+import { App } from "./App.js";
 
-
-// const tag = document.createElement('script');
-// tag.src = './devtools/lightning-inspect.js';
-// tag.onload = () => console.log('Inspector script loaded successfully');
-// tag.onerror = () => console.error('Failed to load inspector script');
-// document.body.appendChild(tag);
-
-
-const app = Launch(App, {
-  stage: {},
-  debug: true,
-  enablePointer: true,
-  keys: {
-    38: 'Up',
-    40: 'Down',
-    37: 'Left',
-    39: 'Right',
-    13: 'Enter',
-    8: 'Back',
-    27: 'Exit',
-  },
-}, {
-  // showFps: true,
-  // inspector: true,
-  // "showVersion": true,
-});
-
-const canvas = app.stage.getCanvas();
-document.body.appendChild(canvas);
+export default function (
+  appSettings: Lightning.Application.Options,
+  platformSettings: PlatformSettings,
+  appData: AppData
+) {
+  return Launch(App, appSettings, platformSettings, appData);
+}
